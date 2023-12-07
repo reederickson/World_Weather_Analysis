1dd9ab93f2a6b98eea618c44cb775d21f290151a
reederickson <reed.erickson19@gmail.com>
2023-12-07 10:42:35 -0800

    Adding API keys

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "ad0c596bbda5ab55cc2bb14a921b47c6"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "dce372ea823d497a84f52bc38c2f94ef"
